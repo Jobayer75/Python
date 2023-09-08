32090316573cd7b8ffeaae058b8977eac4d7548e
Jobayer75 <jobayerjoyaddar@gmail.com>
2023-09-09 01:01:38 +0800

    Python beginners

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Hello world")
+print("   / |")
+print("  /  |")
+print(" /   |")
+print("/    |")
+character_name="Badhon"
+chharacter_age="25"
+
+print("My name is " + character_name + ",")
+print("My age is " + chharacter_age + ",")
+
+character_name="Trading"
+print("My hobby is " + character_name + ",")
+print("I am pasionate to do more and more business")
+
+Phrase="Jobayer Joyaddar"
+print(Phrase + " Badhon ")
+print(Phrase.upper())
+print(Phrase.lower())
