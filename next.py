53eb3451de28608c728bc1d052edf896743bc25c
Jobayer75 <jobayerjoyaddar@gmail.com>
2023-09-09 19:18:52 +0800

    Python practice

---
+++ next.py
@@ -0,0 +1,26 @@
+color=input("Enter a color :")
+plural_noun=input("Enter a plural noun :")
+celebrity=input("Enter a celebrity: ")
+
+print("Shirts are " + color)
+print(plural_noun + " are black ")
+print("I love " + celebrity)
+
+
+
+Friends=["Sazzad" , "Sagar" , "Turjo" , "Tanzir"]
+print(Friends[1])
+print(Friends[2:])
+
+lucky_numbers=[4,6,8,9,3]
+Friends=["Sazzad" , "Sagar" , "Turjo" , "Tanzir"]
+Friends.append("Alvi")
+Friends.insert(2 ,"Zayn")
+Friends.remove("Alvi")
+Friends.extend(lucky_numbers)
+print(Friends.index("Sagar"))
+Friends.count([1])
+Friends.pop()
+print(Friends)
+
+
