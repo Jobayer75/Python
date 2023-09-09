02ecacc066b848240e26109026f16fb33921f5ed
Jobayer joyaddar <66490138+Jobayer75@users.noreply.github.com>
2023-09-09 19:54:10 +0800

    Delete adv directory

--- adv/18_-_while_loop.py
+++
@@ -1,7 +0,0 @@
-# "while" loop is executed while the logical condition are met.
-i = 1
-while i <= 10:
-    print(i)
-    i += 1
-
-print("Done with loop.")
--- adv/More.py
+++
@@ -1,6 +0,0 @@
-print("Tuple")
-coordinates=[(6,7) , (4,5) , (2,3)]
-print(coordinates[1])
-
-print("Function")
-
